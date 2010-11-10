a87a455e6b1b04a045540f282e958ae0078ea730
Falko <filatau@gmail.com>
2010-11-10 01:07:00 -0800

    Deleted incorrectly named wish files

--- app/controllers/wish_controller.rb
+++
@@ -1,9 +0,0 @@
-class WishController < ApplicationController
-
-  def create
-	@user = User.find(params[:user_id])
-	@wish = @user.wishes.create[:idea]
-	redirect_to user_path(@user)
-  end
-  
-end
